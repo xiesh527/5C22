7171612ef557eeebcba32ffabf4e61af0a065a24
Xie <xiesh@AD.MEE.TCD.IE>
2022-11-09 11:08:05 +0000

    Correct the illness that python starts from 1 not 0

--- SUM_first_odd.py
+++ SUM_first_odd.py
@@ -1,7 +1,10 @@
 import numpy as np
 
+#This function computes the sum of first odd numbers
 def sum_f_odd(N):
     sum = 0
+    count = 1
     for i in N:
-        sum = sum + (2*i - 1)
+        sum = sum + (2*count - 1)
+        count = count + 1
     return sum
